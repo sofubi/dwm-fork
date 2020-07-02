7192b06a9ba0a0f525844f74ba346b7b99efcfd5
Thomas Lawton <thomas@makeros.com>
2020-07-02 09:22:12 -0400

    Adds keys for general programs, alacritty instead of st

--- config.def.h
+++ config.def.h
@@ -57,13 +57,13 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -94,6 +94,10 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,						XK_b,      spawn,          {.v = firefox } },
+	{ MODKEY,						XK_s,      spawn,          {.v = slack } },
+	{ MODKEY,						XK_d,      spawn,          {.v = discord-canary } },
+	{ MODKEY,						XK_g,      spawn,          {.v = gitkraken } },
 };
 
 /* button definitions */
